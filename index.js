add04f8b9966bb7a474deb667ff15a1ffc60b631
bonowricardo1-cell <bonowricardo1@gmail.com>
2026-08-22 22:40:22 -0300

    Update index.js

--- index.js
+++ index.js
@@ -183,3 +183,14 @@ client.on('interactionCreate', async (interaction) => {
 });
 
 client.login(process.env.DISCORD_TOKEN);
+const http = require('http');
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('BLAND bot rodando normal!\n');
+});
+
+server.listen(PORT, () => {
+  console.log(`Servidor HTTP ouvindo na porta ${PORT}`);
+});
